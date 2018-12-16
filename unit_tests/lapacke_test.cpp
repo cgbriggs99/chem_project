5c52a049694637f5cc6a2403f4ba409e64bdb5ee
Tom Briggs <tbriggs@cs.ship.edu>
2018-12-15 16:14:28 -0800

    test DGEEV most working, need to add expected checks

--- unit_tests/lapacke_test.cpp
+++ unit_tests/lapacke_test.cpp
@@ -17,8 +17,9 @@ using namespace std;
 
 class TestBasicMath: public CppUnit::TestFixture {
 CPPUNIT_TEST_SUITE(TestBasicMath);
-	CPPUNIT_TEST(testDGELS);CPPUNIT_TEST_SUITE_END()
-	;
+	CPPUNIT_TEST(testDGELS);
+	CPPUNIT_TEST(testDGEEV);
+	CPPUNIT_TEST_SUITE_END();
 
 //public:
 //    void setUp(void);
@@ -26,6 +27,7 @@ CPPUNIT_TEST_SUITE(TestBasicMath);
 
 protected:
 	void testDGELS(void);
+	void testDGEEV(void);
 
 private:
 //    CBasicMath *mTestObj;
@@ -61,6 +63,78 @@ void TestBasicMath::testDGELS(void) {
 
 }
 
+#define N 5
+#define LDA N
+#define LDVL N
+#define LDVR N
+
+void TestBasicMath::testDGEEV(void) {
+
+
+	// lapack_int LAPACKE_dgeev( int matrix_layout, char jobvl, char jobvr,
+    //lapack_int n, double* a, lapack_int lda, double* wr,
+    //double* wi, double* vl, lapack_int ldvl, double* vr,
+    //lapack_int ldvr );
+
+	lapack_int n = N, lda = LDA, ldvl = LDVL, ldvr = LDVR, info;
+
+	/* Local arrays */
+	double wr[N], wi[N], vl[LDVL*N], vr[LDVR*N];
+	double a[LDA*N] = {
+	   -1.01,  0.86, -4.60,  3.31, -4.81,
+		3.98,  0.53, -7.04,  5.29,  3.55,
+		3.30,  8.26, -3.89,  8.20, -1.51,
+		4.43,  4.96, -7.66, -7.33,  6.18,
+		7.31, -6.43, -6.16,  2.47,  5.58
+	};
+
+	/* Executable statements */
+	printf( "LAPACKE_dgeev (row-major, high-level) Example Program Results\n" );
+
+	/* Solve eigenproblem */
+	info = LAPACKE_dgeev( LAPACK_ROW_MAJOR, 'V', 'V', n, a, lda, wr, wi,
+					vl, ldvl, vr, ldvr );
+
+	double expected_values[9] = {
+			2.86, 10.76,   2.86,-10.76,  -0.69,  4.70,  -0.69, -4.70 -10.46
+	 };
+
+	double expected_left[5][9] = {
+	   0.04,  0.29,   0.04, -0.29,  -0.13, -0.33,  -0.13,  0.33,   0.04,
+	   0.62,  0.00,   0.62,  0.00,   0.69,  0.00,   0.69,  0.00,   0.56,
+	  -0.04, -0.58,  -0.04,  0.58,  -0.39, -0.07,  -0.39,  0.07,  -0.13,
+	   0.28,  0.01,   0.28, -0.01,  -0.02, -0.19,  -0.02,  0.19,  -0.80,
+	  -0.04,  0.34,  -0.04, -0.34,  -0.40,  0.22,  -0.40, -0.22,   0.18
+	  };
+
+	double expected_right[5][9] = {
+	   0.11,  0.17,   0.11, -0.17,   0.73,  0.00,   0.73,  0.00,   0.46,
+	   0.41, -0.26,   0.41,  0.26,  -0.03, -0.02,  -0.03,  0.02,   0.34,
+	   0.10, -0.51,   0.10,  0.51,   0.19, -0.29,   0.19,  0.29,   0.31,
+	   0.40, -0.09,   0.40,  0.09,  -0.08, -0.08,  -0.08,  0.08,  -0.74,
+	   0.54,  0.00,   0.54,  0.00,  -0.29, -0.49,  -0.29,  0.49,   0.16
+	};
+
+
+//	/* Auxiliary routine: printing eigenvectors */
+//	void print_eigenvectors( char* desc, MKL_INT n, double* wi, double* v, MKL_INT ldv ) {
+//	        MKL_INT i, j;
+//	        printf( "\n %s\n", desc );
+	   for(int i = 0; i < N; i++ ) {
+	      int j = 0;
+	      while( j < N ) {
+	         if( wi[j] == (double)0.0 ) {
+	            printf( " %6.2f", vl[i*ldvl+j] );
+	            j++;
+	         } else {
+	            printf( " (%6.2f,%6.2f)", vl[i*ldvl+j], vl[i*ldvl+(j+1)] );
+	            printf( " (%6.2f,%6.2f)", vl[i*ldvl+j], -vl[i*ldvl+(j+1)] );
+	            j += 2;
+	         }
+	      }
+	      printf( "\n" );
+	   }
+}
 
 
 //void TestBasicMath::setUp(void)
